c6cbffa2d76581522447c566e7897fae30602070
DavidS2602 <DavidS260202@proton.me>
2024-05-06 21:57:11 -0600

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('../app/shared/pages/home-page/home-page.component')
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('../app/shared/pages/about-page/about-page.component')
+  }
+];
