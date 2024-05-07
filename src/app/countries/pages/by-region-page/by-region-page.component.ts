4852096fe803f6d735d0a520cd19b1df93726728
DavidS2602 <DavidS260202@proton.me>
2024-05-07 09:13:27 -0600

    Update page titles and add search boxes

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchBoxComponent } from '@shared/components/search-box/search-box.component';
 
 @Component({
   selector: 'app-by-region-page',
   standalone: true,
-  imports: [],
+  imports: [SearchBoxComponent],
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
