703e33f5ff7724bae8d9e3f67fabc145adce7380
DavidS2602 <DavidS260202@proton.me>
2024-05-07 07:35:58 -0600

    Add countries routes, component and relative routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,14 +8,35 @@ export const routes: Routes = [
   },
   {
     path: 'home',
-    loadComponent: () => import('../app/shared/pages/home-page/home-page.component')
+    loadComponent: () => import('@shared/pages/home-page/home-page.component')
   },
   {
     path: 'about',
-    loadComponent: () => import('../app/shared/pages/about-page/about-page.component')
+    loadComponent: () => import('@shared/pages/about-page/about-page.component')
   },
   {
     path: 'contact',
-    loadComponent: () => import('../app/shared/pages/contact-page/contact-page.component')
+    loadComponent: () => import('@shared/pages/contact-page/contact-page.component')
+  },
+  {
+    path: 'countries',
+    children:[
+      {
+        path: 'by-capital',
+        loadComponent: () => import('@countries/pages/by-capital-page/by-capital-page.component')
+      },
+      {
+        path: 'by-region',
+        loadComponent: () => import('@countries/pages/by-region-page/by-region-page.component')
+      },
+      {
+        path: 'by-country',
+        loadComponent: () => import('@countries/pages/by-country-page/by-country-page.component')
+      },
+      {
+        path: 'by/:id',
+        loadComponent: () => import('@countries/pages/country-page/country-page.component')
+      }
+    ]
   }
 ];
