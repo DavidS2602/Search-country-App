5b571b332f88d498d28b62f220d14069cf2abc4a
DavidS2602 <DavidS260202@proton.me>
2024-05-09 09:05:35 -0600

    Add catching possible errors during fetching data

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Country } from '@countries/interfaces/country';
+import { catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,9 @@ export class CountriesService {
 
   searchCapital(capital: string) {
     return this.httpClient.get<Country[]>(`${this.apiUrl}/capital/${capital}`)
+    .pipe(
+      catchError(() => of([]))
+    )
   }
 
 }
