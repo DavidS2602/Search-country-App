af083dcdfad3f1285060b4411906935bd771bf11
DavidS2602 <DavidS260202@proton.me>
2024-05-16 19:16:27 -0600

    chore: Add debounce functionality to search boxes

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,14 +8,37 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit{
+
+
+
+  private debouncer: Subject<string> = new Subject<string>();
+
+
   @Input()
   public placeholder: string = ''
 
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(800) //Despues de 2 segundos pasa al .subscribe
+    )
+    .subscribe(value => {
+      this.onDebounce.emit(value);
+    })
+  }
+
   emitValue(value: string) {
     this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm);
   }
 }
